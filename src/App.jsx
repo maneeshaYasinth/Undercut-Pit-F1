229bca364907a14786708f445cc5eac72e306715
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-06-24 12:34:53 +0530

    removed unnessary codes

--- src/App.jsx
+++ src/App.jsx
@@ -17,37 +17,6 @@ const App = () => {
   const [endDate, setEndDate] = useState(null);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchSessionData = async () => {
-      try {
-        const response = await fetch('https://api.openf1.org/v1/sessions', {
-          params: {
-            session_key: sessionKey
-          }
-        });
-        if (!response.ok) {
-          throw new Error('Failed to fetch session data');
-        }
-        const data = await response.json();
-        if (data && data.length > 0) {
-          const session = data[0];
-          setStartDate(new Date(session.date_start).toISOString());
-          setEndDate(new Date(session.date_end).toISOString());
-        } else {
-          throw new Error('No session data available');
-        }
-      } catch (error) {
-        console.error('Error fetching session data:', error);
-        setError('Error fetching session data');
-      }
-    };
-
-    fetchSessionData();
-  }, [sessionKey]);
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
 
   return (
     <>
